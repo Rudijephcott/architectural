191309fa906968590a26a00bad924b11ed7d0b56
Rudi Jephcott <rudijephcott@gmail.com>
2022-09-09 10:43:29 +0200

    msot of the fckin website i guess

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -17,3 +17,35 @@ function switchTheme(e) {
 }
 
 toggleSwitch.addEventListener("change", switchTheme, false);
+
+$(document).ready(function () {
+  var $carousel = $(".carousel").flickity({
+    cellAlign: "center",
+    contain: true,
+    pageDots: false,
+    prevNextButtons: false,
+    wrapAround: true,
+  });
+  $(".carousel-nav").flickity({
+    asNavFor: ".carousel",
+    contain: true,
+    pageDots: false,
+    wrapAround: true,
+    cellAlign: "center",
+  });
+  // variable for highest height
+  //   let maxHeight = 0;
+
+  //   // defining what im looping through, which in this case is .each .slide inside of .carousel
+  //   $(".carousel .slide-image.match-height").each(function () {
+  //     // this is referring to the slide in question thats being looped through
+  //     let thisHeight = $(this).outerHeight(true);
+  //     // simple if loop
+  //     if (thisHeight < maxHeight) {
+  //       maxHeight = thisHeight;
+  //     }
+  //     console.log(maxHeight);
+  //   });
+
+  //   $(".carousel .slide-image.match-height").css({ height: maxHeight + "px" });
+});
